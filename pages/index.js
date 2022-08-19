7af02b986aa532212caa3e492bed6bbc4431c3b0
Brian Duong <brianminhduong@gmail.com>
2022-08-18 22:42:47 -0400

    Create dummy data for Homepage

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some Address 5, 12345 Some City",
+    description: "This is a first meetup!",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some Address 5, 12345 Some City",
+    description: "This is a first meetup!",
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={}/>;
+}
+
+export default HomePage;
